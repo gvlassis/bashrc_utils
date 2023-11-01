952c5d2a9e4dd3edc8ebb7e1f36f9dae90b18eea
Georgios Vlassis <gvlassis@mailbox.org>
2023-11-01 13:02:53 +0100

    Fixed exception handling and increased TIMEOUT to 3

--- src/GET.py
+++ src/GET.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 import socket
 
-TIMEOUT=2
+TIMEOUT=3
 
 try:
 	ip_api_response = requests.get("http://ip-api.com/json/?fields=city,lat,lon,query", timeout=TIMEOUT)
@@ -13,7 +13,7 @@ ip_api_dict = ip_api_response.json()
 
 try:
 	open_meteo_response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&hourly=temperature_2m,precipitation_probability&timezone=auto&forecast_days=1&current_weather=true" % (ip_api_dict["lat"], ip_api_dict["lon"]), timeout=TIMEOUT)
-except requests.exceptions.ConnectionError:
+except requests.exceptions.RequestException:
 	print("\x1b[91mopen-meteo.com✖\x1b[0m")
 	exit(3)
 open_meteo_dict = open_meteo_response.json()
